ecc8ea0ab278a09c9def4a723ba77987f6a7af0b
Ian Parr <ianjudparr@gmail.com>
2018-10-09 21:08:21 +0100

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+
+import Home from './components/home';
+import About from './components/about';
+import Messages from './components/messages';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload. Hi!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="menu">
+          <ul>
+            <li> <Link to="/">Home</Link> </li>
+            <li> <Link to="/messages">Messages</Link> </li>
+            <li> <Link to="/about">About</Link> </li>
+          </ul>
       </div>
+      <div className="App-intro">
+      <Route exact path="/" component={Home}/>
+          <Route path="/messages" component={Messages}/>
+          <Route path="/about" component={About}/>
+        </div>
+    </div>
     );
   }
 }
