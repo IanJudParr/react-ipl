35f18ee16a53b42bf3bffaef05994200aa550a4e
Ian Parr <ianjudparr@gmail.com>
2018-10-14 20:46:53 +0100

    Add Fromspree to contact form

--- src/components/contactItem.js
+++ src/components/contactItem.js
@@ -8,20 +8,22 @@ export default class ContactItem extends React.Component {
     return (
       <div>
         <div class="col-lg-6 form-group">
-          <form name="contact" method="POST" netlify>
+          <form name="contact" method="POST" action={"https://formspree.io/" + "ianjudparr@gmail.com"}>
             <div class="row">
               <div class="input-group input-group-lg">
                 <span class="input-group-addon" id="basic-addon1">@</span>
-                <input type="text" class="form-control" placeholder="email address" />
+                <input type="email" class="form-control" placeholder="email address" name="_replyto" />
               </div>
             </div>
             <div class="row top-buffer">
-              <textarea class="input-group input-group-lg form-control" rows="5" id="comment"></textarea>
+              <textarea class="input-group input-group-lg form-control" rows="5" id="comment" name="name"></textarea>
             </div>
             <div class="row top-buffer">
-              <button type="button" class="btn btn-lg btn-success pull-right"  type="submit">Send</button>
+              <input type="button" class="btn btn-lg btn-success pull-right"  type="submit" value="Send"/>
             </div>
           </form>
+
+
         </div>
         <div class="col-lg-6 form-group">
           <LocationMap data={this.props.data} />
